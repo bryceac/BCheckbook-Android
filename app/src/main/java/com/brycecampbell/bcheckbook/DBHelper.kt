6fbf73950c8c8397255739a4a5ae16698312b9f9
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-06 11:08:39 -0700

    fix update query.

--- app/src/main/java/com/brycecampbell/bcheckbook/DBHelper.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/DBHelper.kt
@@ -177,7 +177,7 @@ class DBHelper(val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, n
 
             val vendor = record.transaction.vendor
             val memo = record.transaction.memo
-            val amount = if (record.transaction.type == TransactionType.Withdrawal) {
+            val amount = if (record.transaction.type == TransactionType.Withdrawal && record.transaction.amount > 0) {
                 record.transaction.amount*-1
             } else {
                 record.transaction.amount
@@ -188,7 +188,7 @@ class DBHelper(val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, n
                 0
             }
 
-            val updateQuery = "Update trades SET date = $date, check_number = ${checkNumber.toString().uppercase()}, vendor = $vendor, memo = $memo, amount = $amount, category = $category, reconciled = $reconciled WHERE id = $id"
+            val updateQuery = "Update trades SET date = \"$date\", check_number = ${if (checkNumber.toString().uppercase().equals("null", ignoreCase = true)) {"NULL"} else {"${checkNumber.toString()}"}}, vendor = $vendor, memo = $memo, amount = $amount, category = ${if (category.toString().uppercase().equals("null", ignoreCase = true)) {"NULL"} else {"${category.toString()}"}}, reconciled = $reconciled WHERE id = $id"
             db.execSQL(updateQuery)
             db.close()
         }
