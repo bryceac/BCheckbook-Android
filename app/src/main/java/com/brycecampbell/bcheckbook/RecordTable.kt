edd6ede91b15e0ed6430f0754fcccccd3374d5ab
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-09 07:40:48 -0700

    remove unused import.

--- app/src/main/java/com/brycecampbell/bcheckbook/RecordTable.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/RecordTable.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.documentfile.provider.DocumentFile
 import androidx.navigation.NavHostController
 import com.brycecampbell.bcheckbook.ui.theme.BCheckbookTheme
 import me.brycecampbell.bcheck.*
