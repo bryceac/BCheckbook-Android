3fbff584eac669d7b95435c6a17419464175d2b4
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-14 14:45:32 -0700

    try to position indicator better.

--- app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
@@ -1,16 +1,20 @@
 package com.brycecampbell.bcheckbook
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.brycecampbell.bcheckbook.ui.theme.BCheckbookTheme
 
 @Composable
 fun ActivityIndicator(isDisplayed: Boolean) {
     if (isDisplayed) {
-        BCheckbookTheme {
-            Column {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Column(verticalArrangement = Arrangement.Center) {
                 CircularProgressIndicator()
             }
         }
