3836666386b35be92a0c7357b40791e9dc4e59fa
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-14 14:32:19 -0700

    put progress indicator ito a new view.

--- app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
@@ -1,2 +1,14 @@
 package com.brycecampbell.bcheckbook
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivityIndicator(isDisplayed: Boolean) {
+    if (isDisplayed) {
+        Column {
+            CircularProgressIndicator()
+        }
+    }
+}
