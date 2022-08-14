0155a6835e2a16e7022f0a819903003915f5652d
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-14 08:53:17 -0700

    finish implementing base for view model.

--- app/src/main/java/com/brycecampbell/bcheckbook/RecordTableViewModel.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/RecordTableViewModel.kt
@@ -1,4 +1,9 @@
 package com.brycecampbell.bcheckbook
 
-class RecordTableViewModel {
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.ViewModel
+import me.brycecampbell.bcheck.Record
+
+class RecordTableViewModel(val manager: DBHelper? = null, var records: MutableList<Record>, val queryState: MutableState<String>): ViewModel() {
+
 }
