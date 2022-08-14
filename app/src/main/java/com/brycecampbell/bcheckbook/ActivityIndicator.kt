eb9c302a9ba214fc8e53d2c78f7bc48d782143a6
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-14 14:41:15 -0700

    get color scheme right.

--- app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/ActivityIndicator.kt
@@ -2,13 +2,17 @@ package com.brycecampbell.bcheckbook
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.brycecampbell.bcheckbook.ui.theme.BCheckbookTheme
 
 @Composable
 fun ActivityIndicator(isDisplayed: Boolean) {
     if (isDisplayed) {
-        Column {
-            CircularProgressIndicator()
+        BCheckbookTheme {
+            Column {
+                CircularProgressIndicator()
+            }
         }
     }
 }
