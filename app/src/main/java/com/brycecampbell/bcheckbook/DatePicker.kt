af53b5cc05b84f76a2c3aac5508dbd610536650a
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-24 14:28:38 -0700

    try creating a date picker.

--- app/src/main/java/com/brycecampbell/bcheckbook/DatePicker.kt
+++ app/src/main/java/com/brycecampbell/bcheckbook/DatePicker.kt
@@ -1,2 +1,54 @@
 package com.brycecampbell.bcheckbook
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayIn
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DatePicker(selectedDate: MutableState<LocalDate>) {
+    val context = LocalContext.current
+    val expanded = remember { mutableStateOf(false) }
+    val datePicker = DatePickerDialog(context, { _: DatePicker, year: Int, month: Int, day: Int ->
+        selectedDate.value = LocalDate(year, month, day)
+    }, selectedDate.value.year, selectedDate.value.monthNumber, selectedDate.value.dayOfMonth)
+
+    ExposedDropdownMenuBox(expanded.value, onExpandedChange = {
+            expanded.value = !expanded.value
+    }, modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(selectedDate.toString(), onValueChange = {}, label = {
+            Text("date")
+        }, modifier = Modifier.fillMaxWidth().clickable {
+            expanded.value != expanded.value
+        }, trailingIcon = {
+            Icon(Icons.Filled.DateRange, "")
+        }, readOnly = true)
+
+        if (expanded.value) {
+            datePicker.show()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DatePickerPreview() {
+    val date = remember { mutableStateOf(Clock.System.todayIn(TimeZone.currentSystemDefault())) }
+
+    DatePicker(date)
+}
